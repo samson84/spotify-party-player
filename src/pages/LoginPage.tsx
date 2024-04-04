2b8d0e104aa15d09622e919d9510630b4e28a344
Daniel Hatas <samson84@gmail.com>
2024-04-04 22:14:10 +0200

    Configure shadcn, remove some unncessary files Add a test button of shadcn

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,6 @@
 import { Navigate } from "react-router-dom";
 import useSpotify  from "../spotify/useSpotify"
+import { Button } from "@/components/ui/button"
 
 export default function LoginPage() {
   const {login, loggedIn} = useSpotify();
@@ -11,7 +12,7 @@ export default function LoginPage() {
   return (
     <>
       <p>You are not logged in.</p>
-      <button onClick={login}>Login with Spotify 🔑</button>
+      <Button onClick={login} variant="default">Login with Spotify 🔑</Button>
     </>
   )
 }
