4f680b2a542ef983e6dace1a9fa9c5f40d6f445f
Daniel Hatas <samson84@gmail.com>
2024-04-12 00:47:47 +0200

    Refactor AddSong
    Correct the button DOM element warnings.
    
    The DrawerTrigger and DrawerClose components were
    actually buttons. If I want to add a styled Button,
    to them, they triggered this warning.
    
    A controlled drawer with keyboard event handler is
    added to avoid this warning.

--- src/features/player/AddSong.tsx
+++ src/features/player/AddSong.tsx
@@ -1,18 +1,16 @@
 import { Button } from "@/components/ui/button"
 import {
   Drawer,
-  DrawerClose,
   DrawerContent,
   DrawerDescription,
-  DrawerFooter,
   DrawerHeader,
   DrawerTitle,
-  DrawerTrigger,
 } from "@/components/ui/drawer"
 import useSearch from "../spotify/useSearch"
 import { useCallback, useEffect, useMemo, useRef, useState } from "react"
 import { Input } from "@/components/ui/input"
 import SearchedTrack from "./SearchedTrack"
+import { CircleX } from "lucide-react"
 
 function debounce(func: (...args: unknown[]) => unknown, timeout: number = 500) {
   let timer: NodeJS.Timeout | null = null;
@@ -35,7 +33,7 @@ export default function AddSong({ onEnqueue }: AddSongProps) {
   const [open, setOpen] = useState(false);
   const { results, error, loading, search } = useSearch({})
   const inputRef = useRef<HTMLInputElement>(null);
-  
+
   const debounced = useMemo(() => debounce((v) => {
     search(v as string)
   }, 1500), [search]);
@@ -61,13 +59,28 @@ export default function AddSong({ onEnqueue }: AddSongProps) {
     return () => clearTimeout(timeout);
   }, [open])
 
+  useEffect(() => {
+    function escapeHandler(event: KeyboardEvent) {
+      if (event.key === 'Escape') {
+        setOpen(false);
+      }
+    }
+    window.addEventListener('keydown', escapeHandler);
+    return () => window.removeEventListener('keydown', escapeHandler);
+  })
+
   return (
-    <Drawer open={open} onOpenChange={(current) => { setOpen(current) }}>
-      <DrawerTrigger><Button>Add Song</Button></DrawerTrigger>
+    <Drawer open={open}>
+      <Button onClick={() => setOpen(true)}>Add Song</Button>
       <DrawerContent className="h-[90%]">
-        <DrawerHeader>
-          <DrawerTitle>Add a new song</DrawerTitle>
-          <DrawerDescription>Search for a song title or an artist or album.</DrawerDescription>
+        <DrawerHeader className="flex">
+          <div className="flex-grow">
+            <DrawerTitle>Add a new song</DrawerTitle>
+            <DrawerDescription>Search for a song title or an artist or album.</DrawerDescription>
+          </div>
+          <div className="">
+            <Button variant="ghost" onClick={() => setOpen(false)}><CircleX /></Button>
+          </div>
         </DrawerHeader>
         <div className="p-4">
           <Input
@@ -91,11 +104,6 @@ export default function AddSong({ onEnqueue }: AddSongProps) {
             )}
           </div>
         </div>
-        <DrawerFooter>
-          <DrawerClose>
-            <Button variant="outline">Cancel</Button>
-          </DrawerClose>
-        </DrawerFooter>
       </DrawerContent>
     </Drawer>
   )
